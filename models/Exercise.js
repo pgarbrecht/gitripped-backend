a8cdee22f185751c974ec66a9b899de953352eb5
ecampos <ecampos.jr54@gmail.com>
2022-09-21 18:58:00 -0500

    create schema and model

--- models/Exercise.js
+++ models/Exercise.js
@@ -1,3 +1,16 @@
 const mongoose = require('mongoose');
 
+//CREATE SCHEMA
+const exerciseSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    exerciseImage: {type: String, required: true},
+    muscles: {type: String, required: true},
+    notes: {type: String},
+}, {timestamp: true})
+//timestamp will track when the object was made. It will return a date.
+
+//CREATE MODEL
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
 module.exports = Exercise
