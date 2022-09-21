1701df98008c526263bf33c870d0b18e55b4de46
pgarbrecht <philgarbrecht@gmail.com>
2022-09-21 18:13:34 -0500

    established get homepage route

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
 
+// Default route
+app.get('/exercises', (req, res) => {
+    const today = new Date();
+    res.send('server working');
+ });
 
 //Confirmation server is working
 app.listen(PORT, () => {
