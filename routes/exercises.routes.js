36d9240f6e3e5c8d70d0893d4c8b0fa87331eb28
ecampos <ecampos.jr54@gmail.com>
2022-10-01 10:30:50 -0500

    add put route and tested on postman (successful)

--- routes/exercises.routes.js
+++ routes/exercises.routes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const ctrls = require("../controllers")
 
 router.get("/", ctrls.exercises.index)
-// IN POSTMAN: do POST and localhost:3003/exercises/new
 router.post("/", ctrls.exercises.create)
+router.put('/:id', ctrls.exercises.update)
 
 module.exports = router;
